a1e7a1267c9db6ea3f36bc35fe5350e2f5e35f61
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-09 17:25:06 +0330

    👤 Adding 'URLs' of authentication

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,3 +1,41 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+
+app_name = 'accounts'
+
+
+urlpatterns = [
+    path(
+        'register/',
+        views.RegisterView.as_view(),
+        name='register',
+    ),
+    path(
+        'verification-sent/',
+        views.VerificationSentView.as_view(),
+        name='verification_sent',
+    ),
+    path(
+        'resend-activation',
+        views.ResendActivationEmailView.as_view(),
+        name='resend_verification',
+    ),
+    path(
+        'activate/<uidb64>/<token>/',
+        views.ActivationAccountView.as_view(),
+        name='activate',
+    ),
+    path(
+        'login/',
+        views.LoginView.as_view(
+            template_name='accounts/login.html',
+        ),
+        name="login",
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
+]
