20c9d2fe8ae3a87bc7bc772aad1f214bd203e027
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-09 17:26:02 +0330

    👤 edit main 'URLs'

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include('apps.accounts.urls', namespace='accounts')),
     path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
     path("__debug__/", include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
