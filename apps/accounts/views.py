a3e8924da8fcbf851325fe3f66d02865584106e3
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-09 17:25:13 +0330

    👤 All views for authentication

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,147 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.views import View
+from django.views.generic import CreateView, FormView, TemplateView
+from django.contrib.auth.views import LoginView as OldLoginView
 
+from apps.core.mixins import IfAuthenticatedRedirectDashboard
+
+
+from .forms import RegisterForm, ResendActivationEmailForm
+from .tokens import account_activation_token
+from .services import (
+    can_send_activation_email,
+    mark_activation_email_send,
+    send_activation_email,
+)
+
+
+User = get_user_model()
 # Create your views here.
+
+class LoginView(IfAuthenticatedRedirectDashboard, OldLoginView):
+    redirect_authenticated_user = True
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        
+        for field in form.fields.values():
+            field.widget.attrs.update({
+                'class': 'input',
+            })
+
+        form.fields['username'].widget.attrs.update({
+            'placeholder': 'نام کاربری...',
+            'autocomplete': 'username',
+        })
+        form.fields['password'].widget.attrs.update({
+            'placeholder': 'رمز عبور...',
+            'autocomplete': 'password',
+        })
+        
+        return form
+class RegisterView(IfAuthenticatedRedirectDashboard, CreateView):
+    template_name = 'accounts/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('accounts:verification_sent')
+    
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        
+        user.email = user.email.lower().strip()
+        user.is_active = False
+        user.email_verified = False
+        user.save()
+
+        super().form_valid(form)
+        
+        transaction.on_commit(
+            lambda: self._send_activation_email_after_commit(user)
+        )
+        
+        messages.success(
+            self.request,
+            'حساب ساخته شد. لینک فعالسازی به ایمیلت ارسال شد.'
+        )
+        return redirect(self.success_url)
+    
+    def _send_activation_email_after_commit(self, user):
+        if can_send_activation_email(user):
+            send_activation_email(self.request, user)
+            mark_activation_email_send(user)
+
+class ActivationAccountView(IfAuthenticatedRedirectDashboard, View):
+    template_name = 'accounts/activation_invalid.html'
+    
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+        
+        if user is not None and account_activation_token.check_token(user, token):
+            if user.email_verified and user.is_active:
+                messages.info(request, 'اکانت شما قبلا فعال شده است.')
+                return redirect('accounts:login')
+            
+            user.is_active = True
+            user.email_verified = True
+            user.save(update_fields=['is_active', 'email_verified'])
+            
+            messages.success(
+                request,
+                'اکانت شما با موفقیت فعال شد. حالا میتوانید وارد شوید.'
+            )
+            
+            return redirect('accounts:login')
+        return render(request, self.template_name)
+    
+    def get_user(self, uidb64):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            return User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            return None
+
+class VerificationSentView(IfAuthenticatedRedirectDashboard, TemplateView):
+    template_name = 'accounts/verification_sent.html'
+    
+class ResendActivationEmailView(IfAuthenticatedRedirectDashboard, FormView):
+    template_name = 'accounts/resend_activation_email.html'
+    form_class = ResendActivationEmailForm
+    success_url = reverse_lazy('accounts:verification_sent')
+    
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        
+        user = User.objects.filter(
+            email=email,
+            email_verified=False,
+            is_active=False,
+        ).first()
+
+        if user is None:
+            messages.info(
+                self.request,
+                'اگر حسابی با این ایمیل وجود داشته باشد، لینک فعالسازی ارسال میشود.'
+            )
+            return redirect(self.success_url)
+        
+        if not can_send_activation_email(user):
+            messages.warning(
+                self.request,
+                'لینک فعالسازی اخیرا ارسال شده، لطفا کمی صبر کنید.'
+            )
+            return redirect(self.success_url)
+        
+        send_activation_email(self.request, user)
+        mark_activation_email_send(user)
+        
+        
+        messages.success(
+            self.request,
+            'اگر حسابی با این ایمیل وجود داشته باشد، لینک فعالسازی ارسال میشود.'
+        )
+        return redirect(self.success_url)
