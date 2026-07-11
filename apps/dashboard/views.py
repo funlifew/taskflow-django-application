9e0a60ee48afe48f979c12d6050ef20281e1bfb4
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-11 06:42:48 +0330

    📊 Adding dashboard and profile views

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,11 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/dashboard.html'
+    
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/profile.html'
