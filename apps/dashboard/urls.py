5da05934085927b92b9af96157ec662e60f83922
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-13 18:04:51 +0330

    📊 Add ProfileUpdateView to the Dashboard URLs

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -6,4 +6,9 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.DashboardView.as_view(), name='dashboard'),
     path("profile/", views.ProfileView.as_view(), name='profile'),
+    path(
+        'profile/update/',
+        views.ProfileUpdateView.as_view(),
+        name='profile_update',
+    ),
 ]
