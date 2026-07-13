a7acb1cdef1414973d5a6b7df1998bdb5896b4c3
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-13 18:05:50 +0330

    🏢 Make the workspaces models (not completed yet)

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+
+from apps.core.models import TimeStampedModel
 
 # Create your models here.
+
+class Workspace(TimeStampedModel):
+    name = models.CharField(
+        max_length=150
+    )
