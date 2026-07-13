1927c9e8750f6219b6c46df86ba8800fd3da6b9b
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-13 23:47:01 +0330

    ⚙ Adding two mixins just for workspaces

--- apps/core/mixins.py
+++ apps/core/mixins.py
@@ -1,6 +1,30 @@
 from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+
+from apps.workspaces.models import Workspace
 class IfAuthenticatedRedirectDashboard:
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect("dashboard:dashboard")
         return super().dispatch(request, *args, **kwargs)
+    
+class AccessibleWorkspaceMixin(LoginRequiredMixin):
+    def get_queryset(self):
+        return (
+            Workspace.objects
+            .filter(
+                Q(owner=self.request.user)
+                | Q(memberships__user=self.request.user)
+            )
+            .filter(is_archived=False)
+            .select_related('owner')
+            .distinct()
+        )
+
+class OwnerWorkspaceMixin(LoginRequiredMixin):
+    def get_queryset(self):
+        return Workspace.objects.filter(
+            owner=self.request.user,
+            is_archived=False,
+        )
