d88c166143e402ce7285ae871a303dfba42eeef4
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-13 18:01:59 +0330

    👤 Better Admin template for users

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
 from .models import User
 
-# Register your models here.
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        (
+            "TaskFlow Profile",
+            {
+                "fields": (
+                    "email_verified",
+                    "avatar",
+                    "bio",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = DjangoUserAdmin.add_fieldsets + (
+        (
+            "اطلاعات حساب",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "email_verified",
+        "is_active",
+        "is_staff",
+    )
+
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+    )
