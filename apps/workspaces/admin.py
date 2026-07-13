2459775eb60cf879cfbcadc4290bdc5a9aaea6b9
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-13 23:47:27 +0330

    🏢 Adding Workspace models to the admin page

--- apps/workspaces/admin.py
+++ apps/workspaces/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
+from .models import Workspace, WorkspaceMembership
+
 # Register your models here.
+
+class WorkspaceMembershipInline(admin.TabularInline):
+    model = WorkspaceMembership
+    extra = 0
+    autocomplete_fields = ('user', )
+    
+@admin.register(Workspace)
+class WorkspaceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "owner",
+        "is_archived",
+        "created_at",
+    )
+    list_filter = (
+        "is_archived",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "description",
+        "owner__username",
+        "owner__email",
+    )
+    autocomplete_fields = ("owner",)
+    inlines = (WorkspaceMembershipInline,)
+
+@admin.register(WorkspaceMembership)
+class WorkspaceMembershipAdmin(admin.ModelAdmin):
+    list_display = (
+        "workspace",
+        "user",
+        "role",
+        "created_at",
+    )
+    list_filter = ("role",)
+    search_fields = (
+        "workspace__name",
+        "user__username",
+        "user__email",
+    )
+    autocomplete_fields = (
+        "workspace",
+        "user",
+    )
