9f2eaec8db98a115cd515dc1d1a8efad2c285119
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-13 18:05:14 +0330

    📊 Improving views and complete the ProfileUpdateView

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,11 +1,32 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.shortcuts import render
+
+from .forms import ProfileUpdateForm
 
 # Create your views here.
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/dashboard.html'
-    
+
+
+
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/profile.html'
+
+
+class ProfileUpdateView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    UpdateView
+):
+    form_class = ProfileUpdateForm
+    template_name = 'dashboard/profile_update.html'
+    success_url = reverse_lazy('dashboard:profile')
+    success_message = 'پروفایل شما با موفقیت به روزرسانی شد.'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    
