3f5da76edd0f8a4a956efc48b48251dc6fc79283
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-14 14:23:49 +0330

    ⚙ Building some mixins to use with Workspace Viewes

--- apps/core/mixins.py
+++ apps/core/mixins.py
@@ -1,8 +1,10 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.db.models import Q
 
-from apps.workspaces.models import Workspace
+from apps.workspaces.models import Workspace, WorkspaceMembership
+
 class IfAuthenticatedRedirectDashboard:
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -28,3 +30,28 @@ class OwnerWorkspaceMixin(LoginRequiredMixin):
             owner=self.request.user,
             is_archived=False,
         )
+
+class WorkspacePermissionMixin(LoginRequiredMixin):
+    workspace_url_kwarg = 'pk'
+    allowed_roles = []
+    
+    def get_workspace(self):
+        if not hasattr(self, '_workspace'):
+            self._workspace = get_object_or_404(
+                Workspace.objects.select_related('owner'),
+                pk = self.kwargs[self.workspace_url_kwarg],
+                is_archived=False,
+            )
+        
+        return self._workspace
+    
+    def get_membership(self):
+        workspace = self.get_workspace()
+        
+        return WorkspaceMembership.objects.filter(
+            workspace=workspace,
+            user=self.request.user,
+        ).first()
+    
+    
+    def dispatch(self, request, *args, **kwargs)
