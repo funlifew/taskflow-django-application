a89b82d266adfd8dd4c8bde2a27da8c89e40d533
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-16 11:14:16 +0330

    🛹 Creating the Board model

--- apps/boards/models.py
+++ apps/boards/models.py
@@ -1,3 +1,57 @@
+from django.conf import settings
 from django.db import models
 
+from apps.core.models import TimeStampedModel
+from apps.workspaces.models import Workspace
+
 # Create your models here.
+
+class Board(TimeStampedModel):
+    workspace = models.ForeignKey(
+        Workspace,
+        on_delete=models.CASCADE,
+        related_name='boards',
+    )
+    
+    title = models.CharField(
+        max_length=150,
+    )
+    
+    description = models.TextField(
+        blank=True,
+        default="",
+    )
+    
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='created_boards',
+    )
+    
+    is_archived = models.BooleanField(
+        default=False,
+    )
+    
+    class Meta:
+        ordering = (
+            '-updated_at',
+            '-pk',
+        )
+        
+        indexes = [
+            models.Index(
+                fields=(
+                    'workspace',
+                    'is_archived',
+                ),
+                name='boards_ws_archive_idx',
+            ),
+        ]
+        
+        verbose_name = 'برد'
+        verbose_name_plural = 'بردها'
+    
+    def __str__(self):
+        return self.title
