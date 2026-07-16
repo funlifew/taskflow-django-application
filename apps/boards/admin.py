44eceb414410cc6759a9447298766da97429cae5
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2026-07-16 11:14:45 +0330

    🛹 Adding Board model to the admin page

--- apps/boards/admin.py
+++ apps/boards/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
+
+from .models import Board
 # Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "workspace",
+        "created_by",
+        "is_archived",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "is_archived",
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+        "workspace__name",
+        "created_by__username",
+        "created_by__email",
+    )
+
+    autocomplete_fields = (
+        "workspace",
+        "created_by",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    list_select_related = (
+        "workspace",
+        "created_by",
+    )
