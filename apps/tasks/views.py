24d45ffaa9bcb2070259b8f66e71ade63b092717
funlifew <funlifewinpain@gmail.com>
2026-07-22 01:00:27 +0330

    ♻️ Add secure task lifecycle backend

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -237,14 +237,11 @@ class TaskUpdateView(
             column=column,
             is_archived=False,
         )
-        
-        form.instance = locked_task
-        
-        
+
         self.object = form.save(
             commit=False
         )
-        
+
         self.object.full_clean()
         self.object.save()
         
@@ -525,4 +522,304 @@ class TaskMoveView(
             board_pk=board.pk,
             column_pk=target_column.pk,
             task_pk=task.pk,
+        )
+
+
+class ArchivedTaskListView(
+    ColumnObjectMixin,
+    BoardReadRequiredMixin,
+    ListView,
+):
+    model = Task
+    template_name = 'tasks/archived_list.html'
+    context_object_name = 'tasks'
+    paginate_by = 12
+    
+    def get_queryset(self):
+        return (
+            Task.objects
+            .archived()
+            .for_column(
+                self.get_column()
+            )
+            .select_related(
+                'column',
+                'assignee',
+                'created_by',
+            )
+            .order_by(
+                '-updated_at',
+                '-pk',
+            )
+        )
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        current_user_role = self.get_current_user_role()
+        
+        context.update(
+            {
+                'workspace': self.get_workspace(),
+                'column': self.get_board(),
+                'current_user_role': current_user_role,
+                'can_restore_tasks': current_user_role in BOARD_WRITE_ROLES,
+                'can_delete_tasks': current_user_role in BOARD_DELETE_ROLES,
+            }
+        )
+        
+        return context
+
+class TaskArchiveView(
+    TaskObjectMixin,
+    BoardWriteRequiredMixin,
+    View,
+):
+    http_method_names = [
+        'post',
+    ]
+    
+    @transaction.atomic
+    def post(
+        self,
+        request,
+        *args,
+        **kwargs,
+    ):
+        board = get_object_or_404(
+            Board.objects.select_for_update(),
+            pk=self.get_board().pk,
+            workspace=self.get_workspace(),
+            is_archived=False,
+        )
+        
+        column = get_object_or_404(
+            Column.objects.select_for_update(),
+            pk=self.get_column().pk,
+            board=board,
+            is_archived=False,
+        )
+        
+        task = get_object_or_404(
+            Task.objects.select_for_update(),
+            pk=self.kwargs['task_pk'],
+            column=column,
+            is_archived=False,
+        )
+        
+        task.is_archived = True
+        task.save(
+            update_fields=[
+                'is_archived',
+                'updated_at',
+            ]
+        )
+        
+        Task.objects.normalize_positions(
+            column=column,
+        )
+        
+        column.save(
+            update_fields=[
+                'updated_at',
+            ]
+        )
+        
+        board.save(
+            update_fields=[
+                'updated_at',
+            ]
+        )
+        
+        messages.success(
+            request,
+            (
+                f'Task «{task.title}» '
+                "با موفقیت آرشیو شد."
+            ),
+        )
+
+        return redirect(
+            "boards:detail",
+            workspace_pk=board.workspace_id,
+            board_pk=board.pk,
+        )
+
+class TaskRestoreView(
+    ArchivedTaskObjectMixin,
+    BoardWriteRequiredMixin,
+    View,
+):
+    http_method_names = [
+        'post'
+    ]
+    @transaction.atomic
+    def post(
+        self,
+        request,
+        *args,
+        **kwargs,
+    ):
+        board = get_object_or_404(
+            Board.objects.select_for_update(),
+            pk=self.get_board().pk,
+            workspace=self.get_workspace(),
+            is_archived=False,
+        )
+        
+        column = get_object_or_404(
+            Column.objects.select_for_update(),
+            pk=self.get_column().pk,
+            board=board,
+            is_archived=False,
+        )
+        
+        task = get_object_or_404(
+            Task.objects.select_for_update(),
+            pk=self.kwargs['task_pk'],
+            column=column,
+            is_archived=True,
+        )
+
+        task.position = (
+            Task.objects.next_position(
+                column=column,
+            )
+        )
+        
+        task.is_archived = False
+        
+        task.save(
+            update_fields=[
+                'position',
+                'is_archived',
+                'updated_at',
+            ]
+        )
+        
+        board.save(
+            update_fields=[
+                'updated_at',
+            ]
+        )
+        
+        column.save(
+            update_fields=[
+                'updated_at',
+            ]
+        )
+        
+        messages.success(
+            request,
+            (
+                f'Task «{task.title}» '
+                "با موفقیت بازیابی شد."
+            ),
+        )
+
+        return redirect(
+            "tasks:detail",
+            workspace_pk=board.workspace_id,
+            board_pk=board.pk,
+            column_pk=column.pk,
+            task_pk=task.pk,
+        )
+
+
+class TaskDeleteView(
+    ArchivedTaskObjectMixin,
+    BoardDeleteRequiredMixin,
+    DeleteView,
+):
+    model = Task
+    template_name = 'tasks/confirm_delete.html'
+    context_object_name = 'task'
+
+    def get_context_data(
+        self,
+        **kwargs
+    ):
+        context = super().get_context_data(
+            **kwargs
+        )
+        
+        context.update(
+            {
+                'workspace': self.get_workspace(),
+                'board': self.get_board(),
+                'column': self.get_column(),
+                'current_user_role': self.get_current_user_role(),
+            }
+        )
+    
+    @transaction.atomic
+    def form_valid(self, form):
+        board = get_object_or_404(
+            Board.objects.select_for_update(),
+            pk=self.get_board().pk,
+            workspace=self.get_workspace(),
+            is_archived=False,
+        )
+
+        column = get_object_or_404(
+            Column.objects.select_for_update(),
+            pk=self.get_column().pk,
+            board=board,
+            is_archived=False,
+        )
+
+        task = get_object_or_404(
+            Task.objects.select_for_update(),
+            pk=self.object.pk,
+            column=column,
+            is_archived=True,
+        )
+        
+        task_title = task.title
+        task.delete()
+        
+        column.save(
+            update_fields=[
+                'updated_at',
+            ]
+        )
+        
+        board.save(
+            update_fields=[
+                'updated_at',
+            ]
+        )
+        
+        messages.success(
+            self.request,
+            (
+                f'Task «{task_title}» '
+                "برای همیشه حذف شد."
+            ),
+        )
+
+        return redirect(
+            self.get_success_url()
+        )
+    
+    def get_success_url(self):
+        return reverse(
+            "tasks:archived_list",
+            kwargs={
+                "workspace_pk": (
+                    self.kwargs[
+                        "workspace_pk"
+                    ]
+                ),
+                "board_pk": (
+                    self.kwargs[
+                        "board_pk"
+                    ]
+                ),
+                "column_pk": (
+                    self.kwargs[
+                        "column_pk"
+                    ]
+                ),
+            },
         )
