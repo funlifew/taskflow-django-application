bb0e4c35abc7daa1c585ec8f6bf32bf6c53d92bf
funlifew <funlifewinpain@gmail.com>
2026-08-04 10:44:50 +0330

    🔔 Update the context processors to work better

--- apps/notifications/context_processors.py
+++ apps/notifications/context_processors.py
@@ -1,21 +1,32 @@
-from .selectors import get_unread_notifications
+from .selectors import (
+    get_recent_notifications,
+    get_unread_notifications_count,
+)
+
+
+HEADER_NOTIFICATIONS_LIMIT = 6
 
 def notifications_context(request):
     if not request.user.is_authenticated:
         return {
-            (
-                "unread_"
-                "notifications_count"
-            ): 0,
+            'unread_notifications_count': 0,
+            'header_notifications': (),
         }
     
+    header_notifications = list(
+        get_recent_notifications(
+            user=request.user,
+            limit=HEADER_NOTIFICATIONS_LIMIT,
+        )
+    )
+    
     return {
-        (
-            'unread_'
-            'notifications_count'
-        ): (
-            get_unread_notifications(
-                user=request.user
+        'unread_notifications_count': (
+            get_unread_notifications_count(
+                user=request.user,
             )
+        ),
+        'header_notifications': (
+            header_notifications
         ),
     }
